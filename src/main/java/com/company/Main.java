25807192f5f6a4a371d649ed27f169f97a39fab3
canadianbees <88695497+canadianbees@users.noreply.github.com>
2021-09-09 22:45:59 -0400

    Update Main.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -9,7 +9,9 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //create the string
         StringInfo word = new StringInfo();
+        //get the number of characters
         word.NumCharacters();
     }
 
